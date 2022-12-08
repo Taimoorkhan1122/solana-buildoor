ae89bd8f5be3ac7160b0851e85db45bc1f26ca5e
taimoorkhan1122 <khant8k83@gmail.com>
2022-12-08 23:31:44 +0500

    wallet connection added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,37 @@
-import { Box, Center, Spacer, Stack } from "@chakra-ui/react"
-import type { NextPage } from "next"
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import Disconnected from "../components/Disconnected";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Buildoor - 🚀</title>
-        <meta name="description" content="The NFT Collection for Buildoors" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Box w="full" h="calc(100vh)" backgroundPosition="center" bgImage={"url(home-background.svg)"}>
-        <Stack w="full" h="calc(100vh)" justify="center">
-          {/* Navbar */}
-          <Spacer />
-          <Center>
-                {/* if connected first view, otherwise second view */}
-          </Center>
-          <Spacer />
-          <Center>
-            <Box marginBottom={4} color="black">
-              <a href="https://twitter.com/_buildspace">
-                built with @_buildspace
-              </a>
-
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Buildoor - 🚀</title>
+                <meta name="description" content="The NFT Collection for Buildoors" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Box
+                w="full"
+                h="calc(100vh)"
+                backgroundPosition="center"
+                bgImage={"url(home-background.svg)"}
+            >
+                <Stack w="full" h="calc(100vh)" justify="center">
+                    <Navbar />
+                    <Spacer />
+                    <Disconnected />
+                    <Center></Center>
+                    <Spacer />
+                    <Center>
+                        <Box marginBottom={4} color="white">
+                            <a href="https://twitter.com/_buildspace">built with @_buildspace</a>
+                        </Box>
+                    </Center>
+                </Stack>
             </Box>
-          </Center>
-        </Stack>
-      </Box>
-
-     
-    </div>
-  )
+        </div>
+    );
 }
